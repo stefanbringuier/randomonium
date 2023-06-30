5ef77bc9612788f1cf7fb6f90c384e85b0d15a9a
Stefan Bringuier <stefanbringuier@gmail.com>
2023-06-30 16:19:02 -0700

    Some changes and additions to processing.

--- micro_pkgs/md_to_blogger/md_to_html.py
+++ micro_pkgs/md_to_blogger/md_to_html.py
@@ -2,6 +2,16 @@ import requests
 import markdown
 from markdown.extensions import Extension
 from markdown.preprocessors import Preprocessor
+from markdown.postprocessors import Postprocessor
+from markdown.treeprocessors import Treeprocessor
+
+from markdown.inlinepatterns import InlineProcessor
+import xml.etree.ElementTree as etree
+from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import Element, SubElement, tostring
+from xml.dom.minidom import parseString
+
+
 import re
 import sys
 import os
@@ -9,39 +19,58 @@ import base64
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name, TextLexer
 from pygments.formatters import HtmlFormatter, RawTokenFormatter
+from urllib.parse import urlparse
+
 
 class CodeBlockPreprocessor(Preprocessor):
-    CODE_BLOCK_RE = re.compile(r'```(?P<language>\w+)?\n(?P<code>.+?)```', re.DOTALL)
+    CODE_BLOCK_RE = re.compile(r"```(?P<language>\w+)?\n(?P<code>.+?)```", re.DOTALL)
 
     def run(self, lines):
         def repl(m):
-            language = m.group('language')
+            language = m.group("language")
             if language is None:
-                language = 'text'
-            code = m.group('code')
+                language = "text"
+            code = m.group("code")
             lexer = get_lexer_by_name(language, stripall=True)
             formatter = HtmlFormatter(nowrap=True)
             code = highlight(code, lexer, formatter)
-            return '\n\n<pre class="language-%s"><code>%s</code></pre>\n\n' % (language, code)
-        text = '\n'.join(lines)
+            return '\n\n<pre class="language-%s"><code>%s</code></pre>\n\n' % (
+                language,
+                code,
+            )
+
+        text = "\n".join(lines)
         text = self.CODE_BLOCK_RE.sub(repl, text)
-        return text.split('\n')
+        return text.split("\n")
+
 
 class InlineCodePreprocessor(Preprocessor):
-    INLINE_CODE_RE = re.compile(r'`(?P<code>.+?)`', re.DOTALL)
+    INLINE_CODE_RE = re.compile(r"`(?P<code>.+?)`", re.DOTALL)
 
     def run(self, lines):
         def repl(m):
-            code = m.group('code').replace('\n', ' ')
+            code = m.group("code").replace("\n", " ")
             lexer = TextLexer()
             formatter = HtmlFormatter(nowrap=True)
             highlighted_code = highlight(code, lexer, formatter)
+            highlighted_code = highlighted_code.rstrip()  # Remove trailing spaces
             return '<span class="inline-code-highlight">%s</span>' % highlighted_code
-        text = '\n'.join(lines)
-        text = self.INLINE_CODE_RE.sub(repl, text)
-        return text.split('\n')
 
-class ImagePreprocessor(Preprocessor):
+        text = "\n".join(lines)
+        text = self.INLINE_CODE_RE.sub(repl, text)
+        return text.split("\n")
+
+
+class CodeBlockExtension(Extension):
+    def extendMarkdown(self, md):
+        md.preprocessors.register(CodeBlockPreprocessor(md), "code_block", 175)
+        md.preprocessors.register(InlineCodePreprocessor(md), "inline_code", 150)
+
+    # md.preprocessors.register(ImagePreprocessor(md), 'image_preprocessor', 125)
+
+
+# Keep for now. Just does encoding.
+""" class ImagePreprocessor(Preprocessor):
     IMAGE_RE = re.compile(r'!\[(.*?)\]\((.*?)\)')
 
     def run(self, lines):
@@ -71,17 +100,94 @@ class ImagePreprocessor(Preprocessor):
             new_line = self.IMAGE_RE.sub(repl, line)
             new_lines.append(new_line)
 
+        return new_lines 
+
+class ImageExtension(Extension):
+    def extendMarkdown(self, md):
+       md.preprocessors.register(ImagePreprocessor(md), 'image_preprocessor', 125)
+"""
+
+
+class ImagePreprocessor(Preprocessor):
+    """
+    Provides image preprocessing.
+    1. Uses base64 encoding to put image in HTML.
+    2. Structure image in bloggerl like environment.
+    """
+
+    IMAGE_RE = re.compile(r"!\[(.*?)\]\((.*?)\)")
+
+    def run(self, lines):
+        def repl(m):
+            alt_text = m.group(1)
+            image_path = m.group(2)
+
+            if image_path.startswith(("http://", "https://", "www")):
+                response = requests.get(image_path)
+                image_data = response.content
+                image_ext = os.path.splitext(image_path)[1][
+                    1:
+                ]  # image extension from URL
+            else:
+                try:
+                    with open(image_path, "rb") as image_file:
+                        image_data = image_file.read()
+                    image_ext = os.path.splitext(image_path)[1][
+                        1:
+                    ]  # image extension from local file
+                except FileNotFoundError:
+                    return (
+                        m.group()
+                    )  # If the file isn't found, return the original match
+
+            encoded_string = base64.b64encode(image_data).decode()
+            data_url = "data:image/{};base64,{}".format(image_ext, encoded_string)
+
+            table = etree.Element(
+                "table",
+                attrib={
+                    "align": "center",
+                    "cellpadding": "0",
+                    "cellspacing": "0",
+                    "class": "tr-caption-container",
+                    "style": "margin-left: auto; margin-right: auto;",
+                },
+            )
+            tbody = etree.SubElement(table, "tbody")
+            tr1 = etree.SubElement(tbody, "tr")
+            td1 = etree.SubElement(tr1, "td", attrib={"style": "text-align: center;"})
+            img = etree.SubElement(
+                td1, "img", attrib={"border": "0", "src": data_url, "width": "500"}
+            )
+            tr2 = etree.SubElement(tbody, "tr")
+            td2 = etree.SubElement(
+                tr2,
+                "td",
+                attrib={"class": "tr-caption", "style": "text-align: center;"},
+            )
+            # td2.text = alt_text
+            td2.text = markdown.markdown(alt_text)  # Render the alt text as Markdown
+
+            # Convert the element to a string and return
+            return etree.tostring(table, encoding="unicode")
+
+        new_lines = []
+        for line in lines:
+            new_line = self.IMAGE_RE.sub(repl, line)
+            new_lines.append(new_line)
+
         return new_lines
 
-class CodeBlockExtension(Extension):
+
+class ImageExtension(Extension):
     def extendMarkdown(self, md):
-        md.preprocessors.register(CodeBlockPreprocessor(md), 'code_block', 175)
-        md.preprocessors.register(InlineCodePreprocessor(md), 'inline_code', 150)
-        md.preprocessors.register(ImagePreprocessor(md), 'image_preprocessor', 125)
+        image_preprocessor = ImagePreprocessor(md)
+        md.preprocessors.register(image_preprocessor, "image_preprocessor", 125)
+        md.preprocessors.deregister("html_block")
 
 
 class MetaDataPreprocessor(Preprocessor):
-    META_PATTERN = re.compile(r'<!-- META: (.+?) -->')
+    META_PATTERN = re.compile(r"<!-- META: (.+?) -->")
 
     def run(self, lines):
         metadata = {}
@@ -92,9 +198,9 @@ class MetaDataPreprocessor(Preprocessor):
                 meta_data = match.group(1)
                 # Parse the metadata and extract relevant information
                 # For example, split by commas to extract tags
-                key_values = [item.strip() for item in meta_data.split(',')]
+                key_values = [item.strip() for item in meta_data.split(",")]
                 for key_value in key_values:
-                    key, value = key_value.split('=')
+                    key, value = key_value.split("=")
                     metadata[key.strip()] = value.strip()
             else:
                 break  # Stop processing when encountering non-meta line
@@ -103,16 +209,17 @@ class MetaDataPreprocessor(Preprocessor):
         print("Metadata:", metadata)
 
         # Return the remaining lines for further Markdown processing
-        return lines[len(metadata) + 1:]
+        return lines[len(metadata) + 1 :]
+
 
 class MetaDataExtension(Extension):
     def extendMarkdown(self, md):
-        md.preprocessors.register(MetaDataPreprocessor(md), 'meta_data', 0)
+        md.preprocessors.register(MetaDataPreprocessor(md), "meta_data", 0)
 
 
 class RawLinkPreprocessor(Preprocessor):
     # Matches both URLs within markdown links and standalone URLs
-    URL_RE = re.compile(r'(\[([^]]+)\]\((https?://[^\s]+)\))|(https?://[^\s]+)')
+    URL_RE = re.compile(r"(\[([^]]+)\]\((https?://[^\s]+)\))|(https?://[^\s]+)")
 
     def run(self, lines):
         def repl(m):
@@ -131,24 +238,84 @@ class RawLinkPreprocessor(Preprocessor):
 
         return new_lines
 
+
 class RawLinkExtension(Extension):
     def extendMarkdown(self, md):
-        md.preprocessors.register(RawLinkPreprocessor(md), 'raw_link', 100)
+        md.preprocessors.register(RawLinkPreprocessor(md), "raw_link", 100)
+
+
+# Interlink for references
+class ReferencesIdPreprocessor(Preprocessor):
+    def run(self, lines):
+        new_lines = []
+        for line in lines:
+            if line.lower().startswith("# references"):
+                line += " {#references}"
+            new_lines.append(line)
+        return new_lines
+
+
+class ReferencesIdExtension(Extension):
+    def extendMarkdown(self, md):
+        md.preprocessors.register(ReferencesIdPreprocessor(md), "references_id", 50)
+
+
+class StylingPostprocessor(Postprocessor):
+    def run(self, text):
+        text = self.format_text(text)
+        return text
+
+    def format_text(self, text):
+        # Split the document into two parts at the "References" heading
+        parts = re.split(r"(?s)(<h[1-6]>References.*?</h[1-6]>)", text)
+
+        # Apply text replacements to each part separately
+        if len(parts) == 3:
+            parts[0] = re.sub(r"<p>", r'<p style="text-align: justify;">', parts[0])
+            parts[2] = re.sub(
+                r"<p>", r'<p style="text-align: left; font-size: 12px;">', parts[2]
+            )
+
+        return "".join(parts)
+
+
+class StylingExtension(Extension):
+    def extendMarkdown(self, md):
+        md.postprocessors.register(StylingPostprocessor(md), "styling", 175)
+
+
+class StylingExtension(Extension):
+    def extendMarkdown(self, md):
+        md.postprocessors.register(StylingPostprocessor(md), "styling", 175)
 
 
 def process(infile):
-    with open(infile, 'r') as f:
+    with open(infile, "r") as f:
         text = f.read()
-    html = markdown.markdown(text, extensions=[CodeBlockExtension(),
-                                                    RawLinkExtension(),
-                                                    MetaDataExtension(),
-                                                    'fenced_code'])
-    html = html.replace('<p>', '<p style="text-align: justify;">')
+    html = markdown.markdown(
+        text,
+        extensions=[
+            CodeBlockExtension(),
+            RawLinkExtension(),
+            MetaDataExtension(),
+            ImageExtension(),
+            ReferencesIdExtension(),
+            StylingExtension(),  # Add this line
+            "fenced_code",
+            "tables",
+            "footnotes",
+            "admonition",
+        ],
+        extension_configs={"footnotes": {"PLACE_MARKER" : "///Footnotes///"}},
+    )
+
+    # html = html.replace('<p>', '<p style="text-align: justify;">')
     return html
+
 
 if __name__ == "__main__":
     infile = sys.argv[1]
     outfile = sys.argv[2]
     html = process(infile)
-    with open(outfile, 'w') as f:
+    with open(outfile, "w") as f:
         f.write(html)
